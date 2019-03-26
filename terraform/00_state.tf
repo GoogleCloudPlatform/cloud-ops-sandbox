57792209420dcb5c983cad82cc67c47d991974bc
Simon Zeltser <zeltser@google.com>
2019-03-25 19:01:34 -0700

    Fix copyrights and comments in terraform scripts and loadgen

--- terraform/00_state.tf
+++ terraform/00_state.tf
@@ -1,7 +1,20 @@
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # Here we configure the state backend. For this demo we're using the "local"
 # backend which stores everything in a local directory. The config you see below
-# is functionally equivalent to the defaults... in actual use you'd either omit
-# this stanza or configure it differently.
+# is functionally equivalent to the defaults.
 
 terraform {
   backend "local" {
@@ -9,8 +22,8 @@ terraform {
   }
 }
 
-# In a productized version of this, we'd probably store state remotely on a
-# per-instance basis. In that case we'd generate a backend config just before
+# TODO - Consider storing state remotely on a per-instance basis. 
+# In that case we'd generate a backend config just before
 # running terraform that would look something like this:
 
 # terraform {
--- terraform/01_provider.tf
+++ terraform/01_provider.tf
@@ -1,3 +1,17 @@
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # the `provider` block contains the configuration for the provider, including
 # credentials and region config. It can be configured via environment variables
 # or via passing a path to a credentials JSON file.
@@ -5,7 +19,7 @@
 # In this demo we're using Application Default Creds instead, see this for
 # details: https://cloud.google.com/docs/authentication/production
 #
-# If we were to productize this, we would probably configure it via env vars
+# TODO:  we can consider configuring it via env vars
 # that were populated appropriately at runtime.
 
 provider "google" {
