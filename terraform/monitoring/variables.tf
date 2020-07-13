58f09df31da1b83fc321a753df8c2a9c9e4e0d68
Nicholas Lin <linnicholas@google.com>
2020-07-13 20:06:32 +0000

    detailed variables description

--- terraform/monitoring/variables.tf
+++ terraform/monitoring/variables.tf
@@ -1,5 +1,8 @@
 # Creating this variable but leaving it empty means that the user will be
 # prompted for a value when terraform is run
+
+# Currently we need the user to provide some basic information to set up the monitoring examples
+# These will be gone when we merge the monitoring terraform with the provisioning terraform
 variable "external_ip" {
   type        = "string"
   description = "The external IP of the kubernetes cluster. Can be revealed by running kubectl get service frontend-external in Cloud CLI."
