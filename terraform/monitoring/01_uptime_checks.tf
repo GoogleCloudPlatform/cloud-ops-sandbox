ec2615f455edd5a3f46685e4a3d16dce3c08ce23
Nicholas Lin <linnicholas@google.com>
2020-07-14 16:18:59 +0000

    added license headers

--- terraform/monitoring/01_uptime_checks.tf
+++ terraform/monitoring/01_uptime_checks.tf
@@ -1,3 +1,17 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # Here we create a default HTTP Uptime Check on the External IP of our
 # Kubernetes cluster revealed by the Load Balancer
 # Our uptime check has a period of 1 minute and times out after 10 seconds
