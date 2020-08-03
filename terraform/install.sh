c77356fd635426fc66bd23125ec5b2bc1831ba98
Daniel Sanche <d.sanche14@gmail.com>
2020-08-03 15:42:49 -0700

    added function for parsing arguments

--- terraform/install.sh
+++ terraform/install.sh
@@ -232,6 +232,29 @@ displaySuccessMessage() {
     log "********************************************************************************"
 }
 
+parseArguments() {
+while (( "$#" )); do
+  case "$1" in
+    -p|--project|--project_id)
+      if [ -n "$2" ] && [ ${2:0:1} != "-" ]; then
+        project=$2
+        shift 2
+      else
+        echo "Error: Argument for $1 is missing" >&2
+        exit 1
+      fi
+      ;;
+    -*|--*=) # unsupported flags
+      echo "Error: Unsupported flag $1" >&2
+      exit 1
+      ;;
+    *) # ignore positional arguments
+      shift
+      ;;
+  esac
+done
+}
+
 log "Checking Prerequisites..."
 getBillingAccount;
 
