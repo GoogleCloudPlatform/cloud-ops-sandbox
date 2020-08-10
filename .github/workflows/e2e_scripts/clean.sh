4686392e6ebd7b536a2056e5d3147db0c4d4b7a3
Daniel Sanche <d.sanche14@gmail.com>
2020-08-10 12:53:07 -0700

    improved clean script

--- .github/workflows/e2e_scripts/clean.sh
+++ .github/workflows/e2e_scripts/clean.sh
@@ -14,14 +14,14 @@ while [ -n "$CLUSTER_ZONE" ]; do
       echo "deleting cluster"
       gcloud container clusters delete stackdriver-sandbox \
           --project $PROJECT_ID --zone $CLUSTER_ZONE --quiet
-      sleep 5
+      sleep 20
   fi
 done
 
 
+# delete loadgenerator
 LOADGENRATOR="first_run"
 while [ -n "$LOADGENRATOR" ]; do
-  # delete loadgenerator
   read -r LOADGENRATOR ZONE <<< $(gcloud compute instances list \
                                     --filter="name:loadgenerator*" \
                                     --project $PROJECT_ID --format="value(name, zone)")
@@ -47,6 +47,32 @@ done
 # clear logs
 for LOG in $(gcloud logging logs list --project $PROJECT_ID --format="value(NAME)"); do
     echo "deleting $LOG..."
-    gcloud logging logs delete $LOG --project $PROJECT_ID --quiet | true
+    gcloud logging logs delete $LOG --project $PROJECT_ID --quiet
 done
 
+# clear dashboards
+DASHBOARD_LIST="first_run"
+while [ -n "$DASHBOARD_LIST" ]; do
+  DASHBOARD_LIST=$(gcloud monitoring dashboards list --format="value(name)")
+  for DASHBOARD in $DASHBOARD_LIST; do
+    gcloud monitoring dashboards delete $DASHBOARD --quiet
+  done
+done
+
+# clear policies
+POLICY_LIST="first_run"
+while [ -n "$POLICY_LIST" ]; do
+  POLICY_LIST=$(gcloud alpha monitoring policies list --format="value(name)")
+  for POLICY in $POLICY_LIST; do
+    gcloud alpha monitoring policies delete $POLICY --quiet
+  done
+done
+
+# clear notification channels
+CHANNEL_LIST="first_run"
+while [ -n "$CHANNEL_LIST" ]; do
+  CHANNEL_LIST=$(gcloud alpha monitoring channels list --format="value(name)")
+  for CHANNEL in $CHANNEL_LIST; do
+    gcloud alpha monitoring channels delete $CHANNEL --quiet
+  done
+done
