e7c6ebdaea34cd682a36443b5435c642bb8f0901
Daniel Sanche <d.sanche14@gmail.com>
2020-08-14 10:33:12 -0700

    set verbose mode

--- .github/workflows/e2e_scripts/run_install.sh
+++ .github/workflows/e2e_scripts/run_install.sh
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+set -x
 
 # This file sets up and executes an installation inside the
 # custom Cloud Shell container
