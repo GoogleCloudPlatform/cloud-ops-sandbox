039f8422cf6a8cfd46f586ab63a84f14d0b69582
Nick Lin <33471452+nicklin37@users.noreply.github.com>
2020-08-20 10:25:51 -0700

    istio service SLO and alerting policies (#322)
    * rebasing, rebrand edits
    
    * rebasing part 2
    
    * wording change

--- terraform/monitoring/05_alerting_policies.tf
+++ terraform/monitoring/05_alerting_policies.tf
@@ -60,3 +60,49 @@ resource "google_monitoring_alert_policy" "custom_service_latency_slo_alert" {
     mime_type = "text/markdown"
   }
 }
+
+# Create an alerting policy on the Availability SLO for the Istio service.
+# The definition of the SLO can be found in the file '04_slos.tf'.
+# Alerts on error budget burn rate.
+resource "google_monitoring_alert_policy" "istio_service_availability_slo_alert" {
+  count = length(var.istio_services)
+  display_name = "${var.istio_services[count.index].service_name} Availability Alert Policy"
+  combiner     = "AND"
+  conditions {
+    display_name = "SLO burn rate alert for availability SLO with a threshold of ${var.istio_services[count.index].availability_burn_rate}"
+    condition_threshold {
+
+      # This filter alerts on burn rate over the past 60 minutes
+      # The service is defined by the unique Istio string that is automatically created
+      filter     = "select_slo_burn_rate(\"projects/${var.project_id}/services/ist:${var.project_id}-zone-${var.zone}-cloud-ops-sandbox-default-${var.istio_services[count.index].service_id}/serviceLevelObjectives/${google_monitoring_slo.istio_service_availability_slo[count.index].slo_id}\", 60m)"
+      threshold_value = "${var.istio_services[count.index].availability_burn_rate}"
+      comparison = "COMPARISON_GT"
+      duration   = "60s"
+    }
+  }
+  documentation {
+    content = "Availability SLO burn for the ${var.istio_services[count.index].service_name} for the past 60m exceeded ${var.istio_services[count.index].availability_burn_rate}x the acceptable budget burn rate. The service is returning less OK responses than desired. Consider viewing the service logs or custom dashboard to retrieve more information or adjust the values for the SLO and error budget."
+    mime_type = "text/markdown"
+  }
+}
+
+# Create another alerting policy, this time on the SLO for latency for the Istio service.
+# Alerts on error budget burn rate.
+resource "google_monitoring_alert_policy" "istio_service_latency_slo_alert" {
+  count = length(var.istio_services)
+  display_name = "${var.istio_services[count.index].service_name} Latency Alert Policy"
+  combiner     = "AND"
+  conditions {
+    display_name = "SLO burn rate alert for latency SLO with a threshold of ${var.istio_services[count.index].latency_burn_rate}"
+    condition_threshold {
+      filter     = "select_slo_burn_rate(\"projects/${var.project_id}/services/ist:${var.project_id}-zone-${var.zone}-cloud-ops-sandbox-default-${var.istio_services[count.index].service_id}/serviceLevelObjectives/${google_monitoring_slo.istio_service_latency_slo[count.index].slo_id}\", 60m)"
+      threshold_value = "${var.istio_services[count.index].availability_burn_rate}"
+      comparison = "COMPARISON_GT"
+      duration   = "60s"
+    }
+  }
+  documentation {
+    content = "Latency SLO burn for the ${var.istio_services[count.index].service_name} for the past 60m exceeded ${var.istio_services[count.index].latency_burn_rate}x the acceptable budget burn rate. The service is responding slower than desired. Consider viewing the service logs or custom dashboard to retrieve more information or adjust the values for the SLO and error budget."
+    mime_type = "text/markdown"
+  }
+}
