bda2f28e80159ebbf3c748563154c5cdd992928c
Daniel Sanche <sanche@google.com>
2020-08-24 21:30:16 -0700

    Find old projects in destroy.sh (#373)
    * improved destroy script
    * filter out projects not owned by caller

--- terraform/destroy.sh
+++ terraform/destroy.sh
@@ -21,29 +21,50 @@ set -o errexit  # Exit on error
 
 log() { echo "$1" >&2;  }
 
+IFS=$'\n'
+acct=$(gcloud info --format="value(config.account)")
+
 # ensure the working dir is the script's folder
 SCRIPT_DIR=$(realpath $(dirname "$0"))
 cd $SCRIPT_DIR
 
 # find the cloud operations sandbox project id
-found=$(gcloud projects list --filter="id:cloud-ops-sandbox-* AND name='Cloud Operations Sandbox Demo'" --format="value(projectId)")
-if [[ -z "${found}" ]]; then
+filter=$(cat <<-END
+  (id:cloud-ops-sandbox-* AND name='Cloud Operations Sandbox Demo')
+  OR
+  (id:stackdriver-sandbox-* AND name='Stackdriver Sandbox Demo')
+END
+)
+found=$(gcloud projects list --filter="$filter" --format="value(projectId)")
+
+# find projects owned by current user
+for proj in ${found[@]}; do
+  iam_test=$(gcloud projects get-iam-policy "$proj" \
+               --flatten="bindings[].members" \
+               --format="table(bindings.members)" \
+               --filter="bindings.role:roles/owner" 2> /dev/null | grep $acct | cat)
+  if [[ -n "$iam_test" ]]; then
+    create_time=$(gcloud projects describe "$proj" --format="value(create_time.date(%b-%d-%Y))")
+    owned_projects+=("$proj | [$create_time]")
+  fi
+done
+
+# prompt user for a project to delete
+if [[ -z "${owned_projects}" ]]; then
     log "error: no Cloud Operations Sandbox projects found"
     exit 1
-elif [[ $(echo ${found} | wc -w) -gt 1 ]]; then
+else
     log "which Cloud Operations Sandbox project do you want to delete?:"
-    select opt in $found "cancel"; do
+    select opt in $owned_projects "cancel"; do
         if [[ "${opt}" == "cancel" ]]; then
             exit 0
         elif [[ -z "${opt}" ]]; then
             log "invalid response"
         else
-            PROJECT_ID=$opt
+            PROJECT_ID=$(echo $opt | awk '{print $1}')
             break
         fi
     done
-else
-    PROJECT_ID=$found
 fi
 
 # attempt deletion (tool will prompt user for confirmation)
