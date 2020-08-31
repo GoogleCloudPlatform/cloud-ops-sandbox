7268212fa55b30d06316cf5c78f3dd8de7b1d883
Elena Berman <eaberman3@gmail.com>
2020-08-31 18:24:18 -0400

    Converted messages to JSON and added version info

--- terraform/install.sh
+++ terraform/install.sh
@@ -23,6 +23,7 @@ if [[ -n "$DEBUG" ]]; then set -x; fi
 # ensure the working dir is the script's folder
 SCRIPT_DIR=$(realpath $(dirname "$0"))
 cd $SCRIPT_DIR
+VERSION="v0.2.5"
 
 log() { echo "$1" >&2; }
 
@@ -37,7 +38,7 @@ promptForBillingAccount() {
     log ""
     log "To list active billing accounts, run:"
     log "gcloud beta billing accounts list --filter open=true"
-    python telemetry.py event=no-active-billing
+    python telemetry.py event=no-active-billing version=$VERSION
     exit 1;
   fi
 
@@ -158,7 +159,7 @@ createProject() {
       log "If you don't have access to this folder, please make sure to request at:"
       log "go/experimental-folder-access"
       log ""
-      python telemetry.py event=googler-project project=$project_id
+      python telemetry.py event=googler-project project=$project_id version=$VERSION
       select opt in "continue" "cancel"; do
         if [[ "$opt" == "continue" ]]; then
           break;
@@ -169,7 +170,7 @@ createProject() {
       folder_id="262044416022" # /experimental-gke  
       gcloud projects create "$project_id" --name="Cloud Operations Sandbox Demo" --folder="$folder_id"    
     else
-      python telemetry.py event=non-googler-project project=$project_id
+      python telemetry.py event=non-googler-project project=$project_id version=$VERSION
       gcloud projects create "$project_id" --name="Cloud Operations Sandbox Demo"      
     fi;
     # link billing account
@@ -251,10 +252,10 @@ getExternalIp() {
   done;
   if [[ $(curl -sL -w "%{http_code}"  "http://$external_ip" -o /dev/null) -eq 200 ]]; then
       log "Hipster Shop app is available at http://$external_ip"
-      python telemetry.py event=hipstershop-up project=$project_id
+      python telemetry.py event=hipstershop-up project=$project_id version=$VERSION
   else
       log "error: Hipsterhop app at http://$external_ip is unreachable"
-      python telemetry.py event=hipstershop-unavailable project=$project_id
+      python telemetry.py event=hipstershop-unavailable project=$project_id version=$VERSION
   fi
 }
 
@@ -282,7 +283,7 @@ loadGen() {
   done
   if [[ $(curl -sL -w "%{http_code}"  "http://$loadgen_ip:8080" -o /dev/null  --max-time 1) -ne 200 ]]; then
     log "error: load generator unreachable"
-    python telemetry.py event=loadgen-unavailable project=$project_id
+    python telemetry.py event=loadgen-unavailable project=$project_id version=$VERSION
   fi
 }
 
@@ -295,10 +296,10 @@ displaySuccessMessage() {
 
     if [[ -n "${loadgen_ip}" ]]; then
         loadgen_addr="http://$loadgen_ip:8080"
-        python telemetry.py event=loadgen-up project=$project_id
+        python telemetry.py event=loadgen-up project=$project_id version=$VERSION
     else
         loadgen_addr="[not found]"
-        python telemetry.py event=loadgen-unavailable project=$project_id
+        python telemetry.py event=loadgen-unavailable project=$project_id version=$VERSION
     fi
     log ""
     log ""
