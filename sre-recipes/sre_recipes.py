054bbcea1b7940b035a0f06b37566db54d1c6ebd
Tiffany Shen <tishen@google.com>
2020-08-31 23:19:25 +0000

    added the ability to use config map contents in kubernetes manifests

--- sre-recipes/sre_recipes.py
+++ sre-recipes/sre_recipes.py
@@ -13,8 +13,8 @@
 # limitations under the License.
 
 ####################################################################
-## This CLI handles the deploying broken services and rolling
-## broken services back. 
+## This CLI is the interface for sre recipes. It 
+## deploys and rolls back broken services.
 ####################################################################
 
 import os
@@ -22,14 +22,28 @@ import subprocess
 import click
 
 dir_abs_path = os.path.dirname(os.path.abspath(__file__))
-manifest_abs_path = dir_abs_path + '/../kubernetes-manifests/'
+recipe_service_mapping = {'recipe-1': 'frontend'}
+
+
+# Returns the absolute path to the filename given the relative
+# path to the file, filename, and file extension
+def get_relative_path(relative_path, filename, extension):
+    return dir_abs_path + relative_path + filename + extension
+
 
 # Returns the absolute path of the working service's kubernetes manifest
-# and the broken service's kubernetes manifest
-def get_paths(service):
-    working_service_abs_path = manifest_abs_path + service + ".yaml"
-    broken_service_abs_path = manifest_abs_path + service + "srerecipes.yaml"
-    return working_service_abs_path, broken_service_abs_path
+def get_working_path(service):
+    return get_relative_path('/../kubernetes-manifests/', service, '.yaml')
+
+
+# Returns the absolute path of the broken service's kubernetes manifest
+def get_broken_path(service):
+    return get_relative_path('/kubernetes-manifests/', service, '.yaml')
+
+
+# Returns the absolute path of the broken service's kubernetes manifest
+def get_config_map_path(recipe):
+    return get_relative_path('/config-maps/', recipe.replace('-', '_'), '.yaml')
 
 
 # Runs the given command and returns any output and error
@@ -41,43 +55,56 @@ def run_command(command):
 
 # Rolls back the working version of the given service and deploys the
 # broken version of the given service
-def break_func(service):
-    working_service_abs_path, broken_service_abs_path = get_paths(service)
+def break_func(recipe):
+    service = recipe_service_mapping[recipe]
+    working_service_abs_path = get_working_path(service)
+    broken_service_abs_path = get_broken_path(service)
+    recipe_config_map_abs_path = get_config_map_path(recipe)
 
-    print("Rolling back working " + service + " service...")
+    print("Rolling back working service...")
     delete_command = 'kubectl delete -f ' + working_service_abs_path
     output, error = run_command(delete_command)
 
-    print("Deploying broken " + service + " service...")
+    print("Setting up broken service...")
+    create_config_map_command = 'kubectl create configmap ' + recipe \
+        + ' --from-file=' + recipe_config_map_abs_path
+    output, error = run_command(create_config_map_command)
+
+    print("Deploying broken service...")
     apply_command = 'kubectl apply -f ' + broken_service_abs_path
     output, error = run_command(apply_command)
+
+    # cleaning up config map for next run to avoid a double create
+    delete_config_map_command = 'kubectl delete configmap ' + recipe
+    output, error = run_command(delete_config_map_command)
 
 
 # Rolls back the broken version of the given service and deploys the
 # working version of the given service
-def restore(service):
-    working_service_abs_path, broken_service_abs_path = get_paths(service)
+def restore(recipe):
+    service = recipe_service_mapping[recipe]
+    working_service_abs_path = get_working_path(service)
+    broken_service_abs_path = get_broken_path(service)
 
-    print("Rolling back broken " + service + " service...")
+    print("Rolling back broken service...")
     delete_command = 'kubectl delete -f ' + broken_service_abs_path
     output, error = run_command(delete_command)
 
-    print("Deploying working " + service + " service...")
+    print("Deploying working service...")
     apply_command = 'kubectl apply -f ' + working_service_abs_path
     output, error = run_command(apply_command)
 
 
 @click.command()
 @click.argument('action', type=click.Choice(['break', 'restore', 'verify']))
-@click.argument('service', type=click.Choice(['frontend']))
-def main(action, service):
+@click.argument('recipe', type=click.Choice(recipe_service_mapping.keys()))
+def main(action, recipe):
     if action == 'break':
-        break_func(service)
+        break_func(recipe)
     elif action == 'restore':
-        restore(service)
+        restore(recipe)
     elif action == 'verify':
         print('verify!')
 
 if __name__ == "__main__":
     main()
-    
