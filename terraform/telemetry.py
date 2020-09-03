39b00400a900e5a0c5508ec13c4f3c816619bd21
Elena Berman <eaberman3@gmail.com>
2020-09-03 13:45:58 -0400

    Moved back to pub sub and changed image to have env vars

--- terraform/telemetry.py
+++ terraform/telemetry.py
@@ -14,7 +14,7 @@
 
 
 # File usage:
-# python3 telemetry.py session project event version
+# python3 telemetry.py --session=$SESSION --project_id=$PROJECT_ID--event=$EVENT --version=$VERSION
 
 # from google.cloud import pubsub_v1
 from google.cloud import storage, exceptions
@@ -23,11 +23,7 @@ from datetime import datetime
 import sys
 import json
 import hashlib
-import uuid
 import click
-
-def get_uuid():
-    return uuid.uuid4()
 
 # define the following helper functions
 def get_datetime_str():
@@ -42,9 +38,9 @@ def get_id_hash(project_id):
     hashed = m.hexdigest()
     return hashed
 
-def get_telemetry_msg(session, project_id, event, version):
+def get_telemetry_msg(session, project-id, event, version):
     datetime=get_datetime_str()
-    project=get_id_hash(project_id)
+    project=get_id_hash(project-id)
     
     # send in json format
     data = {
@@ -55,38 +51,25 @@ def get_telemetry_msg(session, project_id, event, version):
         "version": version
     }
     msg = json.dumps(data)
-    # file name should be descriptive but unique
-    file_name = 'telemetry-' + project_id + "-" + str(get_uuid())
-    return msg, file_name
+    return msg
 
 @click.command()
 @click.option('--session', help='Current session (unique across projects).')
-@click.option('--bucket_name', help='Name of the bucket provisioned in user project for telemetry storage')
-@click.option('--project_id', help='Project name in Google Cloud Platform.')
+@click.option('--project-id', help='Project id in Google Cloud Platform.')
 @click.option('--event', help='The  event that occurred.')
 @click.option('--version', default="v0.2.5", help='Release version of Sandbox.')
-def store_message(session, bucket_name, project_id, event, version):
-    msg, file_name = get_telemetry_msg(session, project_id, event, version)
+def store_message(session, project-id, event, version):
+    # send data as a bytestring
+    msg = get_telemetry_msg(session, project-id, event, version)
+    print("sending data", data)	
+    data = data.encode("utf-8")
     
-    # create bucket if none created yet
-    storage_client = storage.Client()
-    bucket = None
-    try:
-        bucket = storage_client.get_bucket(bucket_name)
-    except exceptions.NotFound:
-        bucket = storage_client.create_bucket(bucket_name)
-        print("Bucket {} created.".format(bucket_name))
-        
-    blob = bucket.blob(file_name)
-    print(blob)
-    blob.upload_from_string(msg)
-
-    print(
-        "File {} uploaded to {}.".format(
-            file_name, file_name
-        )
-    )
-    
+    # connect to pubsub and send message
+    project_id = "stackdriver-sandbox-230822"
+    topic_id = "telemetry"
+    publisher = pubsub_v1.PublisherClient()
+    topic_path = publisher.topic_path(project_id, topic_id)	topic_path = publisher.topic_path(project_id, topic_id)
+    publisher.publish(topic_path, data=data)
 
 if __name__ == "__main__":
     store_message()
