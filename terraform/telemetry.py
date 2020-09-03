c45605fe64742968eb2e7b56162ce54f25cd046c
Elena Berman <eaberman3@gmail.com>
2020-09-02 20:18:07 -0400

    Added logic to upload to bucket

--- terraform/telemetry.py
+++ terraform/telemetry.py
@@ -42,7 +42,7 @@ def get_id_hash(project_id):
     hashed = m.hexdigest()
     return hashed
 
-def get_telemetry_file(session, project_id, event, version):
+def get_telemetry_msg(session, project_id, event, version):
     datetime=get_datetime_str()
     project=get_id_hash(project_id)
     
@@ -54,13 +54,10 @@ def get_telemetry_file(session, project_id, event, version):
         "datetime": datetime,
         "version": version
     }
-    
+    msg = json.dumps(data)
     # file name should be descriptive but unique
-    file_name = 'telemetry-' + project + "-" + str(get_uuid())
-    with open(file_name, 'w') as file:
-        json.dump(data, file)
-        
-    return file_name
+    file_name = 'telemetry-' + project_id + "-" + str(get_uuid())
+    return msg, file_name
 
 @click.command()
 @click.option('--session', help='Current session (unique across projects).')
@@ -69,7 +66,7 @@ def get_telemetry_file(session, project_id, event, version):
 @click.option('--event', help='The  event that occurred.')
 @click.option('--version', default="v0.2.5", help='Release version of Sandbox.')
 def store_message(session, bucket_name, project_id, event, version):
-    file_name = get_telemetry_file(session, project_id, event, version)
+    msg, file_name = get_telemetry_msg(session, project_id, event, version)
     
     # create bucket if none created yet
     storage_client = storage.Client()
@@ -79,10 +76,10 @@ def store_message(session, bucket_name, project_id, event, version):
     except exceptions.NotFound:
         bucket = storage_client.create_bucket(bucket_name)
         print("Bucket {} created.".format(bucket_name))
-    
+        
     blob = bucket.blob(file_name)
     print(blob)
-    blob.upload_from_filename(file_name)
+    blob.upload_from_string(msg)
 
     print(
         "File {} uploaded to {}.".format(
