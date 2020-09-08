11d9aec25b3f886ee190774a8485153ca855e7a1
Elena Berman <eaberman3@gmail.com>
2020-09-08 14:41:08 -0500

    Added argument validation

--- terraform/telemetry.py
+++ terraform/telemetry.py
@@ -13,13 +13,14 @@
 # limitations under the License.
 
 # File usage:
-# python3 telemetry.py --session=$SESSION --project_id=$PROJECT_ID--event=$EVENT --version=$VERSION
+# python3 telemetry.py --session=$SESSION --project_id=$PROJECT_ID --event=$EVENT --version=$VERSION
 
 import sys
 import json
 import hashlib
 import click
 import time
+import re
 
 from google.cloud import pubsub_v1
 
@@ -44,12 +45,41 @@ def get_telemetry_msg(session, project_id, event, version):
     msg = json.dumps(data)
     return msg
 
+# returns True if arguments are valid (correct format and type)
+# returns False along with an error message if not
+def validate_args(session, project_id, event, version):
+    args_exp = {
+        "version": r"^v\d.\d.\d$",
+        "project_id": r"^cloud-ops-sandbox-(\d){9}$",
+        "v4uuid": r"^[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[89aAbB][a-f0-9]{3}-[a-f0-9]{12}$"
+    }
+    
+    # check types
+    if (not isinstance(event, str) or not isinstance(version, str) or
+        not isinstance(project_id, str) or not isinstance(session, str)):
+        return False, "An argument passed to telemetry was an invalid type."
+    
+    # check format
+    if (re.fullmatch(args_exp["version"], version) is None or
+        re.fullmatch(args_exp["project_id"], project_id) is None or
+        re.fullmatch(args_exp["session"], session) is None):
+        return False, "An argument passed to telemetry was an invalid format."
+    
+    return True, ""
+
 @click.command()
 @click.option('--session', help='Current session (unique across projects).')
 @click.option('--project_id', help='Sandbox project id in Google Cloud Platform.')
 @click.option('--event', help='The  event that occurred.')
-@click.option('--version', default="v0.2.5", help='Release version of Sandbox.')
+@click.option('--version', help='Release version of Sandbox.')
 def send_telemetry_message(session, project_id, event, version):
+    # validate data
+    valid, err_msg = validate_args(session, project_id, event, version)
+    if (not valid):
+        print("Failed to send telemetry.")
+        print(err_msg)
+        return
+    
     # send data as a bytestring
     msg = get_telemetry_msg(session, project_id, event, version)
     bytes = msg.encode("utf-8")
@@ -63,4 +93,4 @@ def send_telemetry_message(session, project_id, event, version):
     publisher.publish(topic_path, data=bytes)
 
 if __name__ == "__main__":
-    send_telmetry_message()
+    send_telemetry_message()
