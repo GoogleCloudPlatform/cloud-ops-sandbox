67648c1c0467ab351bffe3036e52d92eddd2b73e
Ayelet41 <problemsolver@google.com>
2021-04-08 18:16:12 +0300

    Update 00_loadgen.tf
    Testing smaller machine types

--- terraform/loadgen/00_loadgen.tf
+++ terraform/loadgen/00_loadgen.tf
@@ -68,7 +68,7 @@ resource "google_container_cluster" "gke_loadgen" {
   # interesting things.
   node_pool {
     node_config {
-      machine_type = "n1-standard-2"
+      machine_type = "n1-standard-1"
 
       oauth_scopes = [
         "https://www.googleapis.com/auth/cloud-platform"  
