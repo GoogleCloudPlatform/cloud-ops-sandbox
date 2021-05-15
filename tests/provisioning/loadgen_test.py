6379c377c73dadb913ac9c823c7cd7871dd0f866
Gan Tu <github@tugan.dev>
2021-05-14 19:09:55 -0700

    fix: update parameters used for locust test

--- tests/provisioning/loadgen_test.py
+++ tests/provisioning/loadgen_test.py
@@ -81,7 +81,7 @@ class TestLoadGenerator(unittest.TestCase):
         if pattern:
             # reset deployment to use new pattern
             set_env_command = "kubectl set env deployment/loadgenerator " \
-                                f"LOCUST_TASK={pattern}_locustfile.py"
+                                f"LOCUST_TASK={pattern}"
             delete_pods_command = "kubectl delete pods -l app=loadgenerator"
             wait_command = "kubectl wait --for=condition=available" \
                             " --timeout=500s deployment/loadgenerator"
