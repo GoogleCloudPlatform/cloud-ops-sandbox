ff717840c80d57ab53e0fe6587e91d574275ebe2
Yoshi Yamaguchi <145104+ymotongpoo@users.noreply.github.com>
2021-05-24 12:19:25 +0900

    Fix undefined error in main_test.py and lint

--- tests/ratingservice/main_test.py
+++ tests/ratingservice/main_test.py
@@ -12,20 +12,19 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from __future__ import print_function
-
+import decimal
+import json
 import os
 import sys
+import time
 import unittest
+
 import requests
 from requests.exceptions import RequestException
 from urllib3.exceptions import ReadTimeoutError
-import json
-import decimal
 
 
 class TestEndpoints(unittest.TestCase):
-
     @classmethod
     def composeUrl(cls, resource, eid=""):
         if not eid:
@@ -37,20 +36,20 @@ class TestEndpoints(unittest.TestCase):
     def setUpClass(cls):
         cls.TIMEOUT = 5.0
 
-        cls.service_url = ''
+        cls.service_url = ""
         if len(sys.argv) > 1:
-            cls.service_url = sys.argv[1].rstrip('/')
+            cls.service_url = sys.argv[1].rstrip("/")
 
         cls.products = []
-        path = os.getcwd().rstrip('/') + '/src/productcatalogservice/products.json'
+        path = os.getcwd().rstrip("/") + "/src/productcatalogservice/products.json"
         if len(sys.argv) > 2:
             path = sys.argv[2]
         try:
             with open(path) as f:
                 data = json.load(f)
-                for product in data['products']:
-                    cls.products.append(product['id'])
-        except:
+                for product in data["products"]:
+                    cls.products.append(product["id"])
+        except Exception:
             print("failed to load product ids from ", path)
             cls.products = []
 
@@ -68,15 +67,24 @@ class TestEndpoints(unittest.TestCase):
         self.session.close()
 
     def sendRequest(self, method, url, **kwargs):
-        headers = {'Accept': 'application/json',
-                   'Accept-Encoding': '', 'User-Agent': None}
+        headers = {
+            "Accept": "application/json",
+            "Accept-Encoding": "",
+            "User-Agent": None,
+        }
         lastError = None
         retries = 3
         while retries > 0:
             try:
                 if method == "GET":
-                    kwargs.setdefault('allow_redirects', True)
-                return self.session.request(method, url, timeout=TestEndpoints.TIMEOUT, headers=headers, **kwargs)
+                    kwargs.setdefault("allow_redirects", True)
+                return self.session.request(
+                    method,
+                    url,
+                    timeout=TestEndpoints.TIMEOUT,
+                    headers=headers,
+                    **kwargs
+                )
             except (RequestException, ReadTimeoutError) as err:
                 lastError = err
                 time.sleep(1)
@@ -84,17 +92,17 @@ class TestEndpoints(unittest.TestCase):
         raise lastError
 
     def testGetAllRatings(self):
-        """ test getting all ratings """
+        """test getting all ratings"""
         url = TestEndpoints.composeUrl("ratings")
         res = self.sendRequest("GET", url)
         self.assertEqual(res.status_code, 200)
-        ratings = res.json().get('ratings')
+        ratings = res.json().get("ratings")
         self.assertTrue(type(ratings) == list)
-        ids = [r['id'] for r in ratings]
+        ids = [r["id"] for r in ratings]
         self.assertEqual(set(ids), set(TestEndpoints.products))
 
     def testGetProductRating(self):
-        """ test getting ratings for a shop product """
+        """test getting ratings for a shop product"""
         # use products[0] in "get product rating" test
         test_product_id = TestEndpoints.products[0]
         url = TestEndpoints.composeUrl("rating", test_product_id)
@@ -102,24 +110,21 @@ class TestEndpoints(unittest.TestCase):
         self.assertEqual(res.status_code, 200)
 
     def testGetRatingNotExist(self):
-        """ test getting rating for non-exist product """
+        """test getting rating for non-exist product"""
         url = TestEndpoints.composeUrl("rating", "random")
         res = self.sendRequest("GET", url)
         self.assertEqual(res.status_code, 404)
 
     def testPostNewRating(self):
-        """ test posting new rating to a product """
+        """test posting new rating to a product"""
         # use products[1] in "post new vote" test
         test_product_id = TestEndpoints.products[1]
         url = TestEndpoints.composeUrl("rating")
-        res = self.sendRequest("POST", url, json={
-            'rating': 5,
-            'id': test_product_id
-        })
+        res = self.sendRequest("POST", url, json={"rating": 5, "id": test_product_id})
         self.assertEqual(res.status_code, 200)
 
     def testNewRatingCalculation(self):
-        """ test rating calculation after recollection """
+        """test rating calculation after recollection"""
         # use products[2] to avoid counting new vote from testRate() test
         test_product_id = TestEndpoints.products[2]
         new_rating_vote = 5
@@ -130,10 +135,9 @@ class TestEndpoints(unittest.TestCase):
         # get current rating / post new vote / recollect / get updated rating
         result1 = self.sendRequest("GET", url_get)
         self.assertEqual(result1.status_code, 200)
-        result2 = self.sendRequest("POST", url_post, json={
-            'rating': new_rating_vote,
-            'id': test_product_id
-        })
+        result2 = self.sendRequest(
+            "POST", url_post, json={"rating": new_rating_vote, "id": test_product_id}
+        )
         self.assertEqual(result2.status_code, 200)
         result2 = self.sendRequest("POST", url_recollect)
         self.assertEqual(result2.status_code, 200)
@@ -141,19 +145,22 @@ class TestEndpoints(unittest.TestCase):
         self.assertEqual(result2.status_code, 200)
 
         data = result1.json()
-        prev_vote = data['votes']
-        prev_rating = decimal.Decimal(data['rating'])
+        prev_vote = data["votes"]
+        prev_rating = decimal.Decimal(data["rating"])
         data = result2.json()
-        new_vote = data['votes']
-        new_rating = decimal.Decimal(data['rating'])
+        new_vote = data["votes"]
+        new_rating = decimal.Decimal(data["rating"])
         self.assertEqual(new_vote, prev_vote + 1)
-        expected_rating = prev_rating + \
-            ((new_rating_vote-prev_rating)/(prev_vote+1))
+        expected_rating = prev_rating + (
+            (new_rating_vote - prev_rating) / (prev_vote + 1)
+        )
         # compare expected result rounded to 4 decimal places
         QUANTIZE_VALUE = decimal.Decimal("0.0001")
-        self.assertEqual(new_rating.quantize(QUANTIZE_VALUE),
-                         expected_rating.quantize(QUANTIZE_VALUE))
+        self.assertEqual(
+            new_rating.quantize(QUANTIZE_VALUE),
+            expected_rating.quantize(QUANTIZE_VALUE),
+        )
 
 
-if __name__ == '__main__':
-    unittest.main(argv=['first-arg-is-ignored'])
+if __name__ == "__main__":
+    unittest.main(argv=["first-arg-is-ignored"])
