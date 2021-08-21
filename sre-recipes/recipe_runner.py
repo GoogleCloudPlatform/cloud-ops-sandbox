fa4985e440ef3c597b463781b34b0c2dd685eb29
Gan Tu <github@tugan.dev>
2021-08-20 19:09:33 -0700

    refactor: improve multiple choice logic

--- sre-recipes/recipe_runner.py
+++ sre-recipes/recipe_runner.py
@@ -55,9 +55,9 @@ class RecipeRunner:
     def run_hint(self):
         hint = self.config.get("hint", None)
         if hint:
-          print(f'Here is your hint!\n\n{hint}')
+            print(f'Here is your hint!\n\n{hint}')
         else:
-          print("This recipe has no hints.")
+            print("This recipe has no hints.")
 
     def run_verify(self):
         verify_config = self.config.get("verify", {})
@@ -69,89 +69,33 @@ class RecipeRunner:
         if affected_service_config:
             if "answer" not in affected_service_config:
                 logging.error(
-                    "Correct answer is not specified for affected_service quiz.")
+                    "Correct answer is not specified for affected service quiz.")
+                exit(1)
+            elif "choices" not in affected_service_config:
+                logging.error(
+                    "No answer choices configured in affected service quiz.")
                 exit(1)
             self.__run_interactive_multiple_choice(
                 "Which service has an issue?",
-                affected_service_config.get("choices", []),
+                affected_service_config.get["choices"],
                 affected_service_config["answer"])
 
         incident_cause_config = verify_config.get("incident_cause", {})
         if incident_cause_config:
             if "answer" not in incident_cause_config:
                 logging.error(
-                    "Correct answer is not specified for incident_cause quiz.")
+                    "Correct answer is not specified for incident cause quiz.")
+                exit(1)
+            elif "choices" not in incident_cause_config:
+                logging.error(
+                    "No answer choices configured in incident cause quiz.")
                 exit(1)
             self.__run_interactive_multiple_choice(
-                "Which was the cause of the issue?",
-                incident_cause_config.get("choices", []),
+                "What was the cause of the issue?",
+                incident_cause_config.get["choices"],
                 incident_cause_config["answer"])
 
     ########################## Recipe Action Handlers ##########################
-
-    def __run_interactive_multiple_choice(self, prompt, choices, correct_answer):
-        """Runs an interactive multiple choice Quiz.
-
-        Example Layout:
-            ===============================================================
-                                MULTIPLE CHOICE QUIZ
-            ===============================================================
-            Question: Which service has an issue?
-            Choices
-              0: Ad
-              1: Cart
-              2: Checkout
-              3: Currency
-              4: Email
-              5: Frontend
-              6: Payment
-              7: Product Catalog
-              8: Rating
-              9: Recommendation
-              10: Shipping
-            Enter your answer: 
-
-        Paramters
-        ---------
-        prompt: string
-            The question prompt to display.
-        choices: string[]
-            A list of potential answers to choose from.
-        correct_answer: int
-            The (0-based) index for the correct answer in the `choices` params.
-        """
-        if not choices:
-            logging.info("Skipped. Empty multiple choice.")
-            return
-
-        if correct_answer < 0 or correct_answer >= len(choices):
-            logging.error(
-                "Correct answer is not in the pool of potential answers.")
-            exit(1)
-
-        # Show the question
-        print("===============================================================")
-        print("                     MULTIPLE CHOICE QUIZ")
-        print("===============================================================")
-        print(f"Question: {prompt}")
-        print("Choices")
-        for i, choice in enumerate(choices):
-            print(f"  {i}: {choice}")
-
-        # Asks for answer
-        while True:
-            user_answer = input("Enter your answer: ").strip()
-            try:
-                user_answer = int(user_answer)
-                if user_answer < 0 or user_answer >= len(choices):
-                    print("[Error] Not a valid choice")
-                elif user_answer == correct_answer:
-                    print("Congratulations! You are correct.")
-                    return
-                else:
-                    print("Incorrect. Please try again.")
-            except ValueError:
-                print("Please enter the number of your selected answer.")
 
     def __run_shell_command(self, command, decode_output=True):
         """
@@ -196,3 +140,67 @@ class RecipeRunner:
                     exit(1)
             else:
                 raise NotImplementedError(f"action not supported: {action}")
+
+    def __run_interactive_multiple_choice(self, prompt, choices, correct_answer):
+        """Runs an interactive multiple choice Quiz.
+
+        Example Layout:
+            ===============================================================
+                                MULTIPLE CHOICE QUIZ
+            ===============================================================
+            Question: Which service has an issue?
+            Choices
+              0: Ad
+              1: Cart
+              2: Checkout
+              3: Currency
+              4: Email
+              5: Frontend
+              6: Payment
+              7: Product Catalog
+              8: Rating
+              9: Recommendation
+              10: Shipping
+            Enter your answer: 
+
+        Paramters
+        ---------
+        prompt: string
+            The question prompt to display.
+        choices: string[]
+            A list of potential answers to choose from.
+        correct_answer: int
+            The (0-based) index for the correct answer in the `choices` params.
+        """
+        if not choices:
+            logging.info("Skipped. Empty multiple choice.")
+            return
+
+        if correct_answer < 0 or correct_answer >= len(choices):
+            logging.error(
+                "Correct answer is not in the pool of potential answers.")
+            exit(1)
+
+        # Show the question
+        print("===============================================================")
+        print("                     MULTIPLE CHOICE QUIZ                      ")
+        print("===============================================================")
+        print(f"Question: {prompt}")
+        print("Choices")
+        for i, choice in enumerate(choices):
+            print(f"  {i}: {choice}")
+
+        # Asks for answer
+        while True:
+            user_answer = input("Enter your answer: ").strip()
+            try:
+                user_answer = int(user_answer)
+                if user_answer < 0 or user_answer >= len(choices):
+                    print("Not a valid choice")
+                elif user_answer == correct_answer:
+                    print("Congratulations! You are correct.")
+                    return
+                else:
+                    print("Incorrect. Please try again.")
+            except ValueError:
+                print("Please enter the number of your selected answer.")
