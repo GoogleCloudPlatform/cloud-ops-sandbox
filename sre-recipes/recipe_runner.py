0ad0639bab975d2ddec0303deac1fba222630336
Gan Tu <github@tugan.dev>
2021-08-20 17:24:27 -0700

    feat: add recipe runner without verify implementation

--- sre-recipes/recipe_runner.py
+++ sre-recipes/recipe_runner.py
@@ -15,7 +15,7 @@
 # -*- coding: utf-8 -*-
 
 from os import path
-import utils
+import subprocess
 import logging
 import yaml
 
@@ -24,7 +24,7 @@ class RecipeRunner:
 
     def __init__(self, recipe_name):
         filepath = path.join(path.dirname(
-            path.abspath(__file__)), "configs", f"{recipe_name}.yaml")
+            path.abspath(__file__)), f"recipes/configs/{recipe_name}.yaml")
         with open(filepath, "r") as file:
             self.recipe = yaml.safe_load(file.read())
 
@@ -40,77 +40,72 @@ class RecipeRunner:
     def config(self):
         return self.recipe.get("config", {})
 
-    @property
-    def hint(self):
-        return self.config.get("hint", "This recipe has no hints.")
-
     ############################ Run Recipe ###################################
 
     def run_break(self):
         print('Deploying broken service...')
-        self.handle_actions(self.config.get("break", []))
+        self.__run_steps(self.config.get("break", {}))
         print('Done. Deployed broken service')
 
     def run_restore(self):
         print('Restoring broken service...')
-        self.handle_actions(self.config.get("restore", []))
+        self.__run_steps(self.config.get("restore", {}))
         print('Done. Restored broken service to working state.')
 
     def run_hint(self):
-        print(f'Here is your hint!\n\n{self.hint}')
+        hint = self.config.get("hint", "This recipe has no hints.")
+        print(f'Here is your hint!\n\n{hint}')
 
     def run_verify(self):
         raise NotImplementedError()
 
     ########################## Recipe Action Handlers ##########################
 
-    def handle_actions(self, actions):
-        for action in actions:
-            action_type = action.get("type", None)
-            if action_type == 'set_env_variable':
-                self.handle_set_env_variable(action)
-            elif action_type == 'update_gcloud_scheduler':
-                self.handle_update_gcloud_scheduler(action)
-            else:
-                logging.error(
-                    f"Unimplemented action '{action_type}' found in '{self.name}'")
-                exit(1)
+    def __run_shell_command(self, command, decode_output=True):
+        """
+        Runs the given command and returns any output and error
+        If `decode_output` is True, try to decode output with UTF-8 encoding,
+        as well as removing any single quote.
+        """
+        process = subprocess.Popen(
+            command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True
+        )
+        output, error = process.communicate()
+        if output is not None and decode_output:
+            output = output.decode("utf-8").replace("'", '').strip()
+        return output, error
 
-    def handle_set_env_variable(self, action):
-        for field in ["service", "selector", "feature_flag", "feature_value"]:
-            if field not in action:
-                logging.error(
-                    f"Can't run set_env_variable. Missing '{field}' flag in config")
-                exit(1)
-
-        utils.set_env_vars(
-            action["service"],
-            f'{action["feature_flag"]}={action["feature_value"]}')
-
-        pod_name, _ = utils.get_pod_name_by_selector(
-            action["selector"])
-        if not pod_name:
+    def __run_steps(self, steps):
+        if type(steps) != list:
             logging.error(
-                f"Can't run set_env_variable. Failed to get pod name")
+                f"Expect `steps` to be list. Found {type(steps)}: {steps}")
+            exit(1)
+        elif not steps:
+            logging.error("There are no steps configured.")
             exit(1)
 
-        utils.delete_pod_by_name(pod_name)
-        utils.wait_for_service(
-            action["service"], action.get("restart_wait_seconds", 600),
-            condition="available")
+        for step in steps:
+            if type(step) != dict:
+                raise ValueError(
+                    f"Expect `step` to be dict. Found {type(step)}")
+            logging.info(f"Runing step: {step}")
+            if "run" in step:
+                output, err = self.__run_shell_command(step["run"])
+                if err:
+                    logging.error(f"Failed to run step {step}: {err}")
+                    exit(1)
+            else:
+                raise NotImplementedError(f"Step not supported: {step}")
 
-    def handle_update_gcloud_scheduler(self, action):
-        raise NotImplementedError()
 
-
-runner = RecipeRunner("recipe0")
-# print(runner.config)
-# print(utils.get_project_id())
-print(runner.name)
-print(runner.description)
-# print(runner.break_config)
-# print(runner.restore_config)
-runner.run_hint()
-# print(runner.verify_config)
-# print(runner.run_break())
-# print(runner.run_restore())
+# runner = RecipeRunner("recipe1")
+# # print(runner.config)
+# # print(utils.get_project_id())
+# print(runner.name)
+# print(runner.description)
+# # print(runner.break_config)
+# # print(runner.restore_config)
+# runner.run_hint()
+# # print(runner.verify_config)
+# runner.run_break()
+# runner.run_restore()
