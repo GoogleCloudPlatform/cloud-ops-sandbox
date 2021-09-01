e252f2afa0ad7860e5a8580d9ade52636dcfaef8
Ayelet41 <problemsolver@google.com>
2021-09-01 10:45:44 +0100

    Revert "fix(terraform): Update provider version constraints"
    This reverts commit 63252185c36acab67c5491652a111b4ea6aacae5.

--- terraform/monitoring/01_provider.tf
+++ terraform/monitoring/01_provider.tf
@@ -22,11 +22,12 @@
 # TODO:  we can consider configuring it via env vars
 # that were populated appropriately at runtime.
 
-terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = ">=3.23.0"
-    }
-  }
+provider "google" {
+  # pin provider to 3.23.0
+  version = ">=3.23.0"
+
+  # credentials = "/path/to/creds.json"
+  project = var.project_id
+  # region = "default-region"
+  # zone = "default-zone"
 }
