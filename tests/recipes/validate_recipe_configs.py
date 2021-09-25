9644b6c1ce4a5002c3134a162f27aff7a485fe3c
Gan Tu <github@tugan.dev>
2021-09-24 20:07:44 -0700

    chore: do not use f-string syntax

--- tests/recipes/validate_recipe_configs.py
+++ tests/recipes/validate_recipe_configs.py
@@ -6,14 +6,14 @@ import jsonschema
 
 RECIPES_ROOT = os.path.join(os.path.dirname(
     os.path.abspath(__file__)), "../../sre-recipes/recipes/configs_based")
-SCHEMA_ROOT = f"{RECIPES_ROOT}/schema"
+SCHEMA_ROOT = RECIPES_ROOT + "/schema"
 
 print("Loading base schema...")
-with open(f"{SCHEMA_ROOT}/schema.json", "r") as file:
+with open(SCHEMA_ROOT + "/schema.json", "r") as file:
     schema = json.load(file)
 
 print("Bundling additional schema definitions...")
-for def_path in glob.glob(f"{SCHEMA_ROOT}/defs/*.schema.json"):
+for def_path in glob.glob(SCHEMA_ROOT + "/defs/*.schema.json"):
     def_name = os.path.basename(def_path).split(".")[0]
     with open(def_path, "r") as def_file:
         def_schema = json.load(def_file)
@@ -23,22 +23,22 @@ for def_path in glob.glob(f"{SCHEMA_ROOT}/defs/*.schema.json"):
 
 print("Running validations on all SRE recipe configs...")
 has_error = False
-for recipe_path in glob.glob(f"{RECIPES_ROOT}/*.yaml"):
+for recipe_path in glob.glob(RECIPES_ROOT + "/*.yaml"):
     recipe_name = os.path.basename(recipe_path).split(".")[0]
-    print(f"Validating {recipe_name}")
+    print("Validating:", recipe_name)
     with open(recipe_path, "r") as recipe_file:
         try:
             recipe_config = yaml.load(
                 recipe_file.read(), Loader=yaml.FullLoader)
         except Exception as e:
-            print(f"Invalid or empty YAML: {e}")
+            print("Invalid or empty YAML:", e)
             has_error = True
             continue
     try:
         jsonschema.validate(recipe_config, schema)
-        print(f"{recipe_name} is valid!")
+        print("Valid!")
     except Exception as e:
-        print(f"{recipe_name} failed validation!\n {e}")
+        print("Invalid!", e)
         has_error = True
 
 if has_error:
