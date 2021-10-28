2c0e9a82cbb35f186566cec181ef4d4526a33e55
Ayelet41 <problemsolver@google.com>
2021-10-28 14:56:38 +0100

    Revert "update waitfor"
    This reverts commit bd72c442cd28848428d510ae0159693ea7f1218a.

--- terraform/microservices/99_outputs.tf
+++
@@ -1,3 +0,0 @@
-output "service_url" {
-  value = "https://ratingservice-dot-${length(google_app_engine_application.app) > 0 ? google_app_engine_application.app[0].default_hostname : data.external.app_engine_state.result.application_domain}"
-}
