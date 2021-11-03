3afaca042449879f79d5daba6ce9e96087ab1faf
Vishal Patel <5624846+vishal84@users.noreply.github.com>
2021-11-03 11:09:54 -0400

    update provider version

--- terraform/01_provider.tf
+++ terraform/01_provider.tf
@@ -28,7 +28,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">=3.23.0"
+      version = "=3.23.0"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "=3.23.0"
     }
     random = {
       source  = "hashicorp/random"
