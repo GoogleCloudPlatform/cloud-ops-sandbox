838582273e546aa0b6a7128331db86dc69eb2062
Ayelet41 <ayeletsachti@gmail.com>
2021-11-03 19:39:10 +0000

    feat(terraform): separating rating service main tf file to multiple for readability and consistency (#859)
    * feat(terraform): breaking rating service main tf file to multiple for readability and consistency
    * fix(terraform): nit: year 2020 to 21
    * feat(terraform): locals readability change
    feat(terraform): locals readability change
    
    Co-authored-by: Ayelet41 <problemsolver@google.com>
    Co-authored-by: Daniel Sanche <sanche@google.com>

---
+++ terraform/ratingservice/03_cloudsql.tf
@@ -0,0 +1,58 @@
+/**
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+resource "random_string" "suffix_len_4" {
+  upper   = false
+  special = false
+  length  = 4
+}
+
+# provision CloudSQL
+resource "random_password" "db_password" {
+  length  = 16
+  special = false
+}
+
+resource "google_sql_database_instance" "rating_service" {
+  name                = "ratingservice-sql-instance-${random_string.suffix_len_4.result}"
+  database_version    = "POSTGRES_12"
+  deletion_protection = false
+  region              = var.gcp_region_name
+  settings {
+    tier = "db-f1-micro"
+  }
+}
+
+resource "google_sql_user" "default" {
+  name     = "postgres"
+  password = random_password.db_password.result
+  instance = google_sql_database_instance.rating_service.name
+}
+
+resource "google_sql_database" "rating_service" {
+  name     = "rating-db"
+  instance = google_sql_database_instance.rating_service.name
+
+  provisioner "local-exec" {
+    command = "${path.module}/configure_rating_db.sh ${var.gcp_project_id}"
+    environment = {
+      DB_HOST     = google_sql_database_instance.rating_service.connection_name
+      DB_NAME     = google_sql_database.rating_service.name
+      DB_USERNAME = google_sql_user.default.name
+      DB_PASSWORD = google_sql_user.default.password
+    }
+  }
+}
---
+++ terraform/ratingservice/06_cloudscheduler.tf
@@ -0,0 +1,44 @@
+/**
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+resource "google_cloud_scheduler_job" "recollect_job" {
+  name             = "ratingservice-recollect-job"
+  schedule         = "* * * * *" # each minute
+  description      = "recollect recently posted new votes"
+  time_zone        = "Europe/London"
+  region           = var.gcp_region_name
+  attempt_deadline = "340s"
+
+  retry_config {
+    min_backoff_duration = "1s"
+    max_retry_duration   = "10s"
+    max_doublings        = 2
+    retry_count          = 3
+  }
+
+  app_engine_http_target {
+    http_method = "POST"
+
+    app_engine_routing {
+      service = var.service_name
+      version = var.service_version
+    }
+
+    relative_uri = "/ratings:recollect"
+  }
+
+  depends_on = [google_project_service.cloudscheduler, google_app_engine_standard_app_version.ratingservice]
+}
