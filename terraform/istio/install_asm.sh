1dc2385293e25090f2462063e4c5b038d5058d00
Daniel Sanche <d.sanche14@gmail.com>
2022-05-18 13:51:25 -0700

    wrote ASM installations script

--- terraform/istio/install_asm.sh
+++ terraform/istio/install_asm.sh
@@ -18,44 +18,43 @@
 # Download Istio
 
 echo "### "
-echo "### Begin install istio control plane"
+echo "### Begin install ASM control plane"
 echo "### "
 
-# Set vars for DIRs
-ISTIO_VERSION=1.7.1
+
+ASM_VERSION=1.12
+PROJECT_ID=$(gcloud config get-value project)
+CLUSTER_ZONE=$(gcloud container clusters list --filter name=cloud-ops-sandbox --format="json" | jq '.[0].zone')
 
 # Set the working directory to our current directory (/sandbox/terraform/istio)
 export SCRIPTPATH=$(dirname $(realpath $0))
 cd $SCRIPTPATH
 export WORK_DIR=$SCRIPTPATH
 
-echo "Downloading Istio ${ISTIO_VERSION}..."
-curl -L https://git.io/getLatestIstio | ISTIO_VERSION=$ISTIO_VERSION sh -
+echo "Downloading asmcli ${ASM_VERSION}..."
+curl https://storage.googleapis.com/csm-artifacts/asm/asmcli_${ASM_VERSION} > asmcli
+chmod +x asmcli
 
-echo "Moving istioctl into WORKDIR..."
-mv istio-$ISTIO_VERSION/bin/istioctl ${WORK_DIR}
+echo "Installing ASM..."
+./asmcli install \
+  --project_id $PROJECT_ID \
+  --cluster_name cloud-ops-sandbox \
+  --cluster_location $CLUSTER_ZONE \
+  --fleet_id $PROJECT_ID \
+  --output_dir $WORK_DIR/asm_output \
+  --enable_all \
+  --option legacy-default-ingressgateway \
+  --ca mesh_ca \
+  --enable_gcp_components
 
-# Prepare for install
-kubectl create namespace istio-system
+echo "Installing Gateway..."
+GATEWAY_NS=istio-gateway
+kubectl create namespace $GATEWAY_NS
+REVISION=$(kubectl get deploy -n istio-system -l app=istiod -o jsonpath={.items[*].metadata.labels.'istio\.io\/rev'}'{"\n"}')
+kubectl label namespace $GATEWAY_NS istio.io/rev=$REVISION --overwrite
+cd $WORK_DIR/asm_output
+kubectl apply -n $GATEWAY_NS -f samples/gateways/istio-ingressgateway
 
-cd ./istio-${ISTIO_VERSION}/
-kubectl create secret generic cacerts -n istio-system \
-    --from-file=samples/certs/ca-cert.pem \
-    --from-file=samples/certs/ca-key.pem \
-    --from-file=samples/certs/root-cert.pem \
-    --from-file=samples/certs/cert-chain.pem
-
-# cd back into istio/
-cd ${WORK_DIR}
-
-kubectl label namespace default istio-injection=enabled
-kubectl create clusterrolebinding cluster-admin-binding \
-    --clusterrole=cluster-admin \
-    --user=$(gcloud config get-value core/account)
-
-# Can also set flag here if values isn't actually being read...https://istio.io/v1.4/docs/setup/install/istioctl/
-# install using operator config - https://istio.io/docs/setup/install/istioctl/#customizing-the-configuration
-${WORK_DIR}/istioctl manifest install -f ${WORK_DIR}/istio_operator.yaml --skip-confirmation
-
-# apply manifests
-kubectl apply -f ${WORK_DIR}/../../istio-manifests
+echo "Enabling Sidecar Injection..."
+cd $WORK_DIR
+kubectl label namespace default istio-injection- istio.io/rev=$REVISION --overwrite
