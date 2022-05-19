30c67385071d6e073e0053ca1853e88c63268341
Daniel Sanche <d.sanche14@gmail.com>
2022-05-19 15:42:36 -0700

    added IAM permissions to compute engine default service account

--- terraform/03_gke_cluster.tf
+++ terraform/03_gke_cluster.tf
@@ -144,6 +144,29 @@ data "google_compute_default_service_account" "default" {
   ]
 }
 
+# Give service account Observability permissions
+resource "google_project_iam_member" "trace_role" {
+  project = var.gcp_project_id
+  role    = "roles/cloudtrace.admin"
+  member  = "serviceAccount:${data.google_compute_default_service_account.default.email}"
+  depends_on = [data.google_compute_default_service_account.default]
+}
+
+resource "google_project_iam_member" "monitoring_role" {
+  project = var.gcp_project_id
+  role    = "roles/monitoring.editor"
+  member  = "serviceAccount:${data.google_compute_default_service_account.default.email}"
+  depends_on = [data.google_compute_default_service_account.default]
+}
+
+
+resource "google_project_iam_member" "profiler_role" {
+  project = var.gcp_project_id
+  role    = "roles/cloudprofiler.agent"
+  member  = "serviceAccount:${data.google_compute_default_service_account.default.email}"
+  depends_on = [data.google_compute_default_service_account.default]
+}
+
 # Create GSA/KSA binding: let IAM auth KSAs as a svc.id.goog member name
 resource "google_service_account_iam_binding" "set_gsa_binding" {
   service_account_id = data.google_compute_default_service_account.default.name // google_service_account.set_gsa.name
