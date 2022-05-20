3f82af1050564bfea1ee27c572ea4e2b3d1aec52
Daniel Sanche <d.sanche14@gmail.com>
2022-05-20 13:09:39 -0700

    added debugger role

--- terraform/03_gke_cluster.tf
+++ terraform/03_gke_cluster.tf
@@ -167,6 +167,13 @@ resource "google_project_iam_member" "profiler_role" {
   depends_on = [data.google_compute_default_service_account.default]
 }
 
+resource "google_project_iam_member" "debugger_role" {
+  project = data.google_project.project.project_id
+  role    = "roles/clouddebugger.agent"
+  member  = "serviceAccount:${data.google_compute_default_service_account.default.email}"
+  depends_on = [data.google_compute_default_service_account.default]
+}
+
 # Create GSA/KSA binding: let IAM auth KSAs as a svc.id.goog member name
 resource "google_service_account_iam_binding" "set_gsa_binding" {
   service_account_id = data.google_compute_default_service_account.default.name // google_service_account.set_gsa.name
