9be29e92f3bf9a7dd6a1da8687d77ca5e7671ee2
Daniel Sanche <d.sanche14@gmail.com>
2022-05-20 15:47:56 -0700

    added logging IAM

--- terraform/03_gke_cluster.tf
+++ terraform/03_gke_cluster.tf
@@ -174,6 +174,13 @@ resource "google_project_iam_member" "debugger_role" {
   depends_on = [data.google_compute_default_service_account.default]
 }
 
+resource "google_project_iam_member" "logging_role" {
+  project = data.google_project.project.project_id
+  role    = "roles/logging.logWriter"
+  member  = "serviceAccount:${data.google_compute_default_service_account.default.email}"
+  depends_on = [data.google_compute_default_service_account.default]
+}
+
 # Create GSA/KSA binding: let IAM auth KSAs as a svc.id.goog member name
 resource "google_service_account_iam_binding" "set_gsa_binding" {
   service_account_id = data.google_compute_default_service_account.default.name // google_service_account.set_gsa.name
