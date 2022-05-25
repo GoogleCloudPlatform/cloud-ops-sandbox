1559e600c9c5c46ef3453f7bb7d555fe94ee9845
Daniel Sanche <d.sanche14@gmail.com>
2022-05-25 15:26:40 -0700

    feat: simplify service dashboards

--- terraform/monitoring/04_slos.tf
+++ terraform/monitoring/04_slos.tf
@@ -12,88 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Create an SLO for availability for the custom service.
-# For all services other than Frontend SLO is defined as following:
-#   99% of HTTP requests are successful within the past 30 day windowed period
-# For the Frontend service SLO is defined as following:
-#   90% of HTTP requests are successful within the past 30 day windowed period
-
-resource "google_monitoring_slo" "custom_service_availability_slo" {
-  count        = length(var.custom_services)
-  service      = google_monitoring_custom_service.custom_service[count.index].service_id
-  slo_id       = "${google_monitoring_custom_service.custom_service[count.index].service_id}-availability-slo"
-  display_name = "Availability SLO with request base SLI (good total ratio) for ${google_monitoring_custom_service.custom_service[count.index].service_id}"
-
-  # The goal sets our objective for successful requests over the 30 day rolling window period
-  goal                = var.custom_services[count.index].availability_goal
-  rolling_period_days = 30
-
-  request_based_sli {
-    good_total_ratio {
-
-      # The "good" service is the number of 200 OK responses
-      good_service_filter = join(" AND ", [
-        "metric.type=\"istio.io/service/server/request_count\"",
-        "resource.type=\"k8s_container\"",
-        "resource.label.\"cluster_name\"=\"cloud-ops-sandbox\"",
-        "metadata.user_labels.\"app\"=\"${var.custom_services[count.index].service_id}\"",
-        "metric.label.\"response_code\"=\"200\""
-      ])
-
-      # The total is the number of non-4XX responses
-      # We eliminate 4XX responses since they do not accurately represent server-side 
-      # failures and have the possibility of skewing our SLO measurements
-      total_service_filter = join(" AND ", [
-        "metric.type=\"istio.io/service/server/request_count\"",
-        "resource.type=\"k8s_container\"",
-        "resource.label.\"cluster_name\"=\"cloud-ops-sandbox\"",
-        "metadata.user_labels.\"app\"=\"${var.custom_services[count.index].service_id}\"",
-        join(" OR ", ["metric.label.\"response_code\"<\"400\"",
-        "metric.label.\"response_code\">=\"500\""])
-      ])
-
-    }
-  }
-}
-
-# Create another SLO on the custom service this time with respect to latency.
-# For all services other than Frontend SLO is defined as following:
-#   99% of requests return in under 500 ms in the previous 30 days
-# For the Frontend service SLO is defined as following:
-#   90% of requests return in under 500 ms in the previous 30 days
-resource "google_monitoring_slo" "custom_service_latency_slo" {
-  count        = length(var.custom_services)
-  service      = google_monitoring_custom_service.custom_service[count.index].service_id
-  slo_id       = "${google_monitoring_custom_service.custom_service[count.index].service_id}-latency-slo"
-  display_name = "Latency SLO with request base SLI (distribution cut) for ${google_monitoring_custom_service.custom_service[count.index].service_id}"
-
-  goal                = var.custom_services[count.index].latency_goal
-  rolling_period_days = 30
-
-  request_based_sli {
-    distribution_cut {
-
-      # The distribution filter retrieves latencies of requests that returned 200 OK responses
-      distribution_filter = join(" AND ", [
-        "metric.type=\"istio.io/service/server/response_latencies\"",
-        "resource.type=\"k8s_container\"",
-        "resource.label.\"cluster_name\"=\"cloud-ops-sandbox\"",
-        "metric.label.\"response_code\"=\"200\"",
-        "metadata.user_labels.\"app\"=\"${var.custom_services[count.index].service_id}\""
-      ])
-
-      range {
-
-        # By not setting a min value, it is automatically set to -infinity
-        # The units of the upper bound is in ms
-        max = var.custom_services[count.index].latency_threshold
-
-      }
-    }
-  }
-}
-
-# Create an SLO for availability for the Istio service.
+# Create an SLO for availability for the Istio services.
 # Example SLO is defined as following:
 #   90% of HTTP requests are successful within the past 30 day windowed period
 resource "google_monitoring_slo" "istio_service_availability_slo" {
@@ -102,39 +21,20 @@ resource "google_monitoring_slo" "istio_service_availability_slo" {
   count        = length(var.istio_services)
   service      = "ist:${var.project_id}-zone-${var.zone}-cloud-ops-sandbox-default-${var.istio_services[count.index].service_id}"
   slo_id       = "${var.istio_services[count.index].service_id}-availability-slo"
-  display_name = "Availability SLO with request base SLI (good total ratio) for ${var.istio_services[count.index].service_id}"
+  display_name = "${var.istio_services[count.index].availability_goal * 100}% - Availability - Rolling 30 Days - ${var.istio_services[count.index].service_id}"
 
   # The goal sets our objective for successful requests over the 30 day rolling window period
   goal                = var.istio_services[count.index].availability_goal
   rolling_period_days = 30
 
-  request_based_sli {
-    good_total_ratio {
-
-      # The "good" service is the number of 200 OK responses
-      good_service_filter = join(" AND ", [
-        "metric.type=\"istio.io/service/server/request_count\"",
-        "resource.type=\"k8s_container\"",
-        "resource.label.\"cluster_name\"=\"cloud-ops-sandbox\"",
-        "metadata.user_labels.\"app\"=\"${var.istio_services[count.index].service_id}\"",
-        "metric.label.\"response_code\"=\"200\""
-      ])
-
-      # The total is the number of non-4XX responses
-      # We eliminate 4XX responses since they do not accurately represent server-side 
-      # failures and have the possibility of skewing our SLO measurements
-      total_service_filter = join(" AND ", [
-        "metric.type=\"istio.io/service/server/request_count\"",
-        "resource.type=\"k8s_container\"",
-        "resource.label.\"cluster_name\"=\"cloud-ops-sandbox\"",
-        "metadata.user_labels.\"app\"=\"${var.istio_services[count.index].service_id}\"",
-        join(" OR ", ["metric.label.\"response_code\"<\"400\"",
-        "metric.label.\"response_code\">=\"500\""])
-      ])
-
+  basic_sli {
+    availability {
+      enabled = "true"
     }
   }
 }
+
+
 
 # Create an SLO with respect to latency using the Istio service.
 # Example SLO is defined as:
@@ -143,7 +43,7 @@ resource "google_monitoring_slo" "istio_service_latency_slo" {
   count               = length(var.istio_services)
   service             = "ist:${var.project_id}-zone-${var.zone}-cloud-ops-sandbox-default-${var.istio_services[count.index].service_id}"
   slo_id              = "${var.istio_services[count.index].service_id}-latency-slo"
-  display_name        = "Latency SLO with request base SLI (distribution cut) for ${var.istio_services[count.index].service_id}"
+  display_name        = "${var.istio_services[count.index].availability_goal * 100}% - Latency - Rolling 30 days - ${var.istio_services[count.index].service_id}"
   goal                = var.istio_services[count.index].latency_goal
   rolling_period_days = 30
 
@@ -170,7 +70,7 @@ resource "google_monitoring_slo" "istio_service_latency_slo" {
 }
 
 # Rating service availability SLO:
-#   99% of HTTP requests are successful within the past 30 day windowed period
+#   90% of HTTP requests are successful within the past 30 day windowed period
 
 resource "google_monitoring_slo" "rating_service_availability_slo" {
   # Uses ratingservice service that is automatically detected and created when the service is deployed to App Engine
@@ -181,7 +81,7 @@ resource "google_monitoring_slo" "rating_service_availability_slo" {
   display_name = "Rating Service Availability SLO with request base SLI (good total ratio)"
 
   # The goal sets our objective for successful requests over the 30 day rolling window period
-  goal                = 0.99
+  goal                = 0.9
   rolling_period_days = 30
 
   request_based_sli {
@@ -218,7 +118,7 @@ resource "google_monitoring_slo" "rating_service_availability_slo" {
 }
 
 # Rating service latency SLO:
-#   99% of requests that return in under 175 ms in the previous 30 days
+#   90% of requests that return in under 175 ms in the previous 30 days
 
 resource "google_monitoring_slo" "rating_service_latency_slo" {
   # Uses ratingservice service that is automatically detected and created when the service is deployed to App Engine
@@ -228,7 +128,7 @@ resource "google_monitoring_slo" "rating_service_latency_slo" {
   slo_id       = "ratingservice-latency-slo"
   display_name = "Rating Service Latency SLO with request base SLI (distribution cut)"
 
-  goal                = 0.99
+  goal                = 0.9
   rolling_period_days = 30
 
   request_based_sli {
@@ -247,7 +147,7 @@ resource "google_monitoring_slo" "rating_service_latency_slo" {
       range {
         # By not setting a min value, it is automatically set to -infinity
         # The upper bound for latency is in ms
-        max = 500
+        max = 1000
       }
     }
   }
@@ -263,7 +163,7 @@ resource "google_monitoring_slo" "rating_service_freshness_slo" {
   slo_id       = "ratingservice-freshness-slo"
   display_name = "Rating freshness SLO with window based SLI"
 
-  goal                = 0.99
+  goal                = 0.9
   rolling_period_days = 1
 
   windows_based_sli {
