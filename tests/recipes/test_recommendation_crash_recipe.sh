4bf8329496f0c233eaf3fe5fdebb8fdd68abb701
Daniel Sanche <d.sanche14@gmail.com>
2022-06-02 16:41:18 -0700

    changed sre recipe test

--- tests/recipes/test_recommendation_crash_recipe.sh
+++ tests/recipes/test_recommendation_crash_recipe.sh
@@ -29,16 +29,17 @@ curl --show-error --fail $HTTP_ADDR/product/OLJCESPC7Z | grep Typewriter
 
 echo "- breaking sandbox..."
 sandboxctl sre-recipes break recipe3
+sleep 5
 broken_pod=$(kubectl get pods --sort-by=.status.startTime -o jsonpath="{.items[-1].metadata.name}")
 kubectl wait --for=condition=ready --timeout=30s pod $broken_pod
-sleep 10
+sleep 5
 
 echo "- expecting to see 500 error..."
 curl -I --no-fail $HTTP_ADDR/product/OLJCESPC7Z | grep "500 Internal Server Error" 
 
 echo "- checking for expected recomendationservice log..."
 sleep 30
-kubectl logs $broken_pod recommendationservice | grep "invalid literal for int() with base 10: '5.0'"
+kubectl logs -l app=recommendationservice | grep "invalid literal for int() with base 10: '5.0'"
 
 echo "- restoring sandbox"
 sandboxctl sre-recipes restore recipe3
