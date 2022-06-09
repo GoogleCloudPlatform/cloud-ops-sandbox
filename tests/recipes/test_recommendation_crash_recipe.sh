6d68dae4247a55bcdcd5d016605db18b68411351
Daniel Sanche <d.sanche14@gmail.com>
2022-06-09 12:32:41 -0700

    changed wait logic

--- tests/recipes/test_recommendation_crash_recipe.sh
+++ tests/recipes/test_recommendation_crash_recipe.sh
@@ -30,8 +30,7 @@ curl --show-error --fail $HTTP_ADDR/product/OLJCESPC7Z | grep Typewriter
 echo "- breaking sandbox..."
 sandboxctl sre-recipes break recipe3
 sleep 10
-broken_pod=$(kubectl get pods --sort-by=.status.startTime -l app=recommendationservice -o jsonpath="{.items[-1].metadata.name}")
-kubectl wait --for=condition=ready --timeout=30s pod $broken_pod
+kubectl wait --for=condition=ready --timeout=120s pods --all
 
 echo "- expecting to see 500 error..."
 curl -I --no-fail $HTTP_ADDR/product/OLJCESPC7Z | grep "500 Internal Server Error" 
