240d7c22bd17d1551c2ad979194700ece5896e75
Daniel Sanche <d.sanche14@gmail.com>
2022-06-10 13:17:36 -0700

    fixed clean script

--- .github/workflows/e2e_scripts/clean.sh
+++ .github/workflows/e2e_scripts/clean.sh
@@ -111,7 +111,7 @@ for INSTANCE_NAME in $(gcloud sql instances list --project=$PROJECT_ID --format=
 done
 
 # delete GKE hub clusters
-for CLUSTER_NAME in $(gcloud container fleet memberships list --format="value(NAME)"); do
-  echo "deleting GKE hub membership $CLUSTER_NAME"
-  gcloud container fleet memberships delete $CLUSTER_NAME --quiet
+for MEMBER_NAME in $(gcloud container fleet memberships list --format="value(NAME)"); do
+  echo "deleting GKE hub membership $MEMBER_NAME"
+  gcloud beta container hub memberships delete $MEMBER_NAME --quiet
 done
