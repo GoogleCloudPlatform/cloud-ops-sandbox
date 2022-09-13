08b934e326022a9dada7df9fc35f327775905eb4
Drew Brown <drewbr@google.com>
2022-09-13 10:33:28 -0600

    Align pinned terraform provider versions

--- terraform/01_provider.tf
+++ terraform/01_provider.tf
@@ -28,11 +28,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "=3.90.0"
+      version = "~> 3.90.0"
     }
     google-beta = {
       source  = "hashicorp/google"
-      version = "=3.90.0"
+      version = "~> 3.90.0"
     }
     random = {
       source  = "hashicorp/random"
