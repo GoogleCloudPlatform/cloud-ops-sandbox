fc0362e8c5b55f2783b0bc24f065f0892d0933d7
minherz <minherz@gmail.com>
2022-10-11 17:30:29 -0700

    feat: support alerts configurations
    change terraform configuration files to support provisioning custom
    alerty policies based on the config files.
    add test configuration to enable running e2e tests on custom alerts.
    
    Fixes #977

---
+++ terraform/04_alerts.tf
@@ -0,0 +1,41 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+locals {
+  alerts_config = yamldecode(templatefile("${var.cfg_file_location}/alerts.yaml", { project_id = var.project_id }))
+  alerts = {
+    for p in local.alerts_config.alerts : p.name => p
+  }
+}
+
+resource "google_monitoring_alert_policy" "alerts" {
+  for_each     = local.alerts
+  display_name = each.value.display_name
+  # only one condition is supported
+  combiner = "AND"
+  conditions {
+    display_name = each.value.display_name
+    # only threshold condition is supported
+    condition_threshold {
+      filter          = each.value.condition.filter
+      threshold_value = each.value.condition.threshold
+      comparison      = each.value.condition.comparison
+      duration        = each.value.condition.duration
+    }
+  }
+  documentation {
+    content   = each.value.documentation.content
+    mime_type = "text/markdown"
+  }
+}
