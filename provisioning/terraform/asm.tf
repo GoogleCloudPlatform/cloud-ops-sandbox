fb10b47dd287002dd6e60c8f73ed94016be1e70b
minherz <minherz@gmail.com>
2023-04-17 09:54:13 -0700

    feat: replace two scripts by one
    create single `sandboxctl` CLI instead of install.sh and delete.sh.
    add shellcheck linting on shell scripts.
    fix TF output to work also when called after abnormal install process.
    fix asm.tf script to remove deleted cluster from ASM membership.
    make conventional commit bot to ignore PRs

--- provisioning/terraform/asm.tf
+++ provisioning/terraform/asm.tf
@@ -15,15 +15,32 @@
 
 resource "null_resource" "install_asm" {
   count = var.enable_asm ? 1 : 0
+
+  triggers = {
+    project_id       = var.gcp_project_id
+    cluster_name     = google_container_cluster.sandbox.name
+    cluster_location = google_container_cluster.sandbox.location
+  }
+
   provisioner "local-exec" {
     interpreter = ["bash", "-exc"]
-    command     = <<EOF
-./scripts/install_asm.sh --project ${var.gcp_project_id} \
-    --channel ${var.asm_channel} \
-    --cluster_name ${resource.google_container_cluster.sandbox.name} \
-    --cluster_location ${resource.google_container_cluster.sandbox.location}
-EOF
+    command     = <<-EOT
+      ./scripts/install_asm.sh --project ${self.triggers.project_id} \
+        --channel ${var.asm_channel} \
+        --cluster_name ${self.triggers.cluster_name} \
+        --cluster_location ${self.triggers.cluster_location}
+EOT
   }
+
+  provisioner "local-exec" {
+    when    = destroy
+    command = <<-EOT
+      gcloud container fleet memberships unregister ${self.triggers.cluster_name} \
+        --gke-cluster '${self.triggers.cluster_location}/${self.triggers.cluster_name}' \
+        --project=${self.triggers.project_id}
+EOT
+  }
+
   depends_on = [
     resource.google_container_cluster.sandbox,
     module.gcloud,
--- provisioning/terraform/scripts/install_asm.sh
+++ provisioning/terraform/scripts/install_asm.sh
@@ -13,11 +13,18 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-WORK_DIR=$(realpath $(dirname "$0"))
-pushd "$WORK_DIR" > /dev/null
+WORK_DIR=$(realpath "$(dirname "$0")")
 
 SCRIPT_NAME="${0##*/}"; readonly SCRIPT_NAME
 ASM_VERSION=1.16; readonly ASM_VERSION
+
+pushd() {
+  command pushd "$@" > /dev/null || return 0
+}
+
+popd() {
+  command popd > /dev/null || return 0
+}
 
 info() {
   echo "⚙️  ${SCRIPT_NAME}: ${1}" >&2
@@ -101,29 +108,33 @@ parse_args() {
   fi
 }
 
+# shellcheck disable=SC2164
+pushd "${WORK_DIR}"
+
 parse_args "$@"
 
 info "Downloading asmcli version ${ASM_VERSION}"
-curl -s https://storage.googleapis.com/csm-artifacts/asm/asmcli_${ASM_VERSION} > asmcli
+curl -s https://storage.googleapis.com/csm-artifacts/asm/asmcli_"${ASM_VERSION}" > asmcli
 chmod -f +x asmcli
 
 info "Installing managed ASM version ${ASM_VERSION} for GKE cluster ${CLUSTER_NAME} ..."
 
 ./asmcli install \
-  --project_id $PROJECT_ID \
-  --cluster_name ${CLUSTER_NAME} \
-  --cluster_location ${CLUSTER_LOCATION} \
-  --fleet_id ${PROJECT_ID} \
+  --project_id "${PROJECT_ID}" \
+  --cluster_name "${CLUSTER_NAME}" \
+  --cluster_location "${CLUSTER_LOCATION}" \
+  --fleet_id "${PROJECT_ID}" \
   --enable_all \
   --option prometheus-and-stackdriver \
   --managed \
-  --channel ${CHANNEL} \
+  --channel "${CHANNEL}" \
   --use_managed_cni
 
 info "Annotating default namespaces for istio injection ..."
-kubectl label namespace default istio-injection- istio.io/rev=asm-managed-${CHANNEL} --overwrite
+kubectl label namespace default istio-injection- istio.io/rev=asm-managed-"${CHANNEL}" --overwrite
 
 # clean up
 rm ./asmcli
 
+# shellcheck disable=SC2164
 popd
